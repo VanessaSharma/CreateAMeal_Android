dcc33b3932ee2ecfc4d651d688f8f5c9f5096342
Vanessa Palacios Sharma <vanelunapalacios@live.com>
2016-11-18 09:43:50 -0800

    Customize font.

--- app/src/main/java/com/epicodus/createameal/MainActivity.java
+++ app/src/main/java/com/epicodus/createameal/MainActivity.java
@@ -1,20 +1,26 @@
 package com.epicodus.createameal;
 
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     private Button mSignInButton;
     private Button mSignUpButton;
+    private TextView mCreateAMealTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mCreateAMealTextView = (TextView) findViewById(R.id.createAMealTextView);
+        Typeface caviarDreamsFont = Typeface.createFromAsset(getAssets(), "fonts/Caviar_Dreams_Bold.ttf");
+        mCreateAMealTextView.setTypeface(caviarDreamsFont);
         mSignInButton = (Button) findViewById(R.id.signInButton);
         mSignInButton.setOnClickListener(new View.OnClickListener() {
             public void onClick (View v){
