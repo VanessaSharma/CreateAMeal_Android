e4b97f2880008e43eb7677f33f5e6f20ededd7e8
Vanessa Palacios Sharma <vanelunapalacios@live.com>
2016-11-18 09:17:20 -0800

    Create toast for Sign In & Sign Up buttons.

--- app/src/main/java/com/epicodus/createameal/MainActivity.java
+++ app/src/main/java/com/epicodus/createameal/MainActivity.java
@@ -2,12 +2,33 @@ package com.epicodus.createameal;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private Button mSignInButton;
+    private Button mSignUpButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mSignInButton = (Button) findViewById(R.id.signInButton);
+            mSignInButton.setOnClickListener(new View.OnClickListener(){
+                @Override
+                public void onClick(View v){
+                    Toast.makeText(MainActivity.this, "Welcome back!", Toast.LENGTH_LONG).show();
+                }
+            });
+
+        mSignUpButton = (Button) findViewById(R.id.signUpButton);
+            mSignUpButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Toast.makeText(MainActivity.this, "Hello New User!", Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
