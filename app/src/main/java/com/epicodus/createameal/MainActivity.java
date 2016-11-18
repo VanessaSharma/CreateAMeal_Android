84e6dc4a7ef9464139752178d363b3fa9726a46d
Vanessa Palacios Sharma <vanelunapalacios@live.com>
2016-11-18 09:25:21 -0800

    Create intent to redirect to new AccountActivity.java.

--- app/src/main/java/com/epicodus/createameal/MainActivity.java
+++ app/src/main/java/com/epicodus/createameal/MainActivity.java
@@ -1,5 +1,6 @@
 package com.epicodus.createameal;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -14,20 +15,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         mSignInButton = (Button) findViewById(R.id.signInButton);
-            mSignInButton.setOnClickListener(new View.OnClickListener(){
-                @Override
-                public void onClick(View v){
-                    Toast.makeText(MainActivity.this, "Welcome back!", Toast.LENGTH_LONG).show();
-                }
-            });
+        mSignInButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick (View v){
+                Intent intent = new Intent(MainActivity.this, AccountActivity.class);
+                startActivity(intent);
+            }
+        });
 
         mSignUpButton = (Button) findViewById(R.id.signUpButton);
-            mSignUpButton.setOnClickListener(new View.OnClickListener(){
+        mSignUpButton.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v){
-                Toast.makeText(MainActivity.this, "Hello New User!", Toast.LENGTH_LONG).show();
+                Intent intent = new Intent(MainActivity.this, AccountActivity.class);
+                startActivity(intent);
             }
         });
     }
