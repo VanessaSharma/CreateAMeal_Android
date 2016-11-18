c9ab9979615dc2fd0584581c48efe94db92a7b0d
Vanessa Palacios Sharma <vanelunapalacios@live.com>
2016-11-18 12:37:10 -0800

    Add arrayAdapter and mListView in order to display menu to users in UserActivity.java.

--- app/src/main/java/com/epicodus/createameal/UserActivity.java
+++ app/src/main/java/com/epicodus/createameal/UserActivity.java
@@ -4,19 +4,38 @@ package com.epicodus.createameal;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
 
 public class UserActivity extends AppCompatActivity {
     @Bind(R.id.userinfo) TextView mUserInfo;
+    @Bind(R.id.listView) ListView mListView;
+
+    private String[] menu = new String[] {"Recipes", "Grocery List", "Favorites <3", "Add a Recipe"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user);
         ButterKnife.bind(this);
+
+        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, menu);
+        mListView.setAdapter(adapter);
+
+        mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                String menu = ((TextView)view).getText().toString();
+                Toast.makeText(UserActivity.this, menu, Toast.LENGTH_LONG).show();
+            }
+        });
 
         Intent intent = getIntent();
         String userName = intent.getStringExtra("userName");
