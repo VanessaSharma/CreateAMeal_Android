f08012389fc2c7fc77f1334f7acc103ecc514a48
Vanessa Palacios Sharma <vanelunapalacios@live.com>
2016-11-18 12:02:10 -0800

    Implement View.OnClickListener and create If and else statement to navigate to different activities.

--- app/src/main/java/com/epicodus/createameal/MainActivity.java
+++ app/src/main/java/com/epicodus/createameal/MainActivity.java
@@ -12,7 +12,7 @@ import android.widget.Toast;
 import butterknife.Bind;
 import butterknife.ButterKnife;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
      @Bind(R.id.signInButton) Button mSignInButton;
      @Bind(R.id.signUpButton) Button mSignUpButton;
      @Bind(R.id.createAMealTextView) TextView mCreateAMealTextView;
@@ -26,20 +26,20 @@ public class MainActivity extends AppCompatActivity {
         Typeface caviarDreamsFont = Typeface.createFromAsset(getAssets(), "fonts/Caviar_Dreams_Bold.ttf");
         mCreateAMealTextView.setTypeface(caviarDreamsFont);
 
-        mSignInButton.setOnClickListener(new View.OnClickListener() {
-            public void onClick (View v){
-                Intent intent = new Intent(MainActivity.this, LogInActivity.class);
-                startActivity(intent);
-            }
-        });
-
-        mSignUpButton.setOnClickListener(new View.OnClickListener(){
+        mSignInButton.setOnClickListener(this);
+        mSignUpButton.setOnClickListener(this);
+    }
             @Override
-            public void onClick(View v){
-                Intent intent = new Intent(MainActivity.this, AccountActivity.class);
-                startActivity(intent);
-            }
-        });
+            public void onClick (View v){
+                if(v == mSignInButton) {
+                    Intent intent = new Intent(MainActivity.this, LogInActivity.class);
+                    startActivity(intent);
+                } else if (v == mSignUpButton) {
+                        Intent intent = new Intent(MainActivity.this, AccountActivity.class);
+                        startActivity(intent);
+                } else{
+                    Intent intent = new Intent(MainActivity.this, MainActivity.class);
+                }
 
     }
 }
