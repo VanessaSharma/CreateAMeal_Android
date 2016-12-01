9c2545b6603f69c40e90ee684996a2e3b9c4e740
Vanessa Palacios Sharma <vanelunapalacios@live.com>
2016-12-01 15:19:50 -0800

    Create Recipe class model with objects containing data for each recipe.

--- app/src/main/java/com/epicodus/createameal/Recipe.java
+++ app/src/main/java/com/epicodus/createameal/Recipe.java
@@ -1,7 +1,52 @@
 package com.epicodus.createameal;
 
-/**
- * Created by Guest on 12/1/16.
- */
+import java.util.ArrayList;
+
 public class Recipe {
+    private String mName;
+    private ArrayList<String> mIngredients = new ArrayList<>();
+    private double mRating;
+    private String mImageUrl;
+    private String mTime;
+    private ArrayList<String> mNutrition = new ArrayList<>();
+    private String mServings;
+
+
+    public Recipe(String name, ArrayList<String> ingredients, double rating, String imageUrl, String time, ArrayList<String> nutrition, String servings) {
+        this.mName = name;
+        this.mIngredients = ingredients;
+        this.mRating = rating;
+        this.mImageUrl = imageUrl;
+        this.mTime = time;
+        this.mNutrition = nutrition;
+        this.mServings = servings;
+    }
+
+    public String getmName(){
+        return mName;
+    }
+
+    public ArrayList<String> getmIngredients(){
+        return mIngredients;
+    }
+
+    public double getmRating(){
+        return mRating;
+    }
+
+    public String getmImageUrl(){
+        return mImageUrl;
+    }
+
+    public String getmTime(){
+        return mTime;
+    }
+
+    public ArrayList<String> getmNutrition(){
+        return mNutrition;
+    }
+
+    public String getmServings(){
+        return mServings;
+    }
 }
