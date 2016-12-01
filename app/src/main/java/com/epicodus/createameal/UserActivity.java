098d1b36da2c49b9930e0ee9b3b48c4cbb23a3c9
Vanessa Palacios Sharma <vanelunapalacios@live.com>
2016-12-01 09:40:50 -0800

    Create getRecipes callback method in UserActivity.

--- app/src/main/java/com/epicodus/createameal/UserActivity.java
+++ app/src/main/java/com/epicodus/createameal/UserActivity.java
@@ -4,6 +4,7 @@ package com.epicodus.createameal;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -11,12 +12,18 @@ import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.io.IOException;
+
 import butterknife.Bind;
 import butterknife.ButterKnife;
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.Response;
 
 public class UserActivity extends AppCompatActivity {
     @Bind(R.id.userinfo) TextView mUserInfo;
     @Bind(R.id.listView) ListView mListView;
+    public static final String TAG = UserActivity.class.getSimpleName();
 
     private String[] menu = new String[] {"Recipes", "Grocery List", "Favorites <3", "Add a Recipe"};
 
@@ -40,5 +47,25 @@ public class UserActivity extends AppCompatActivity {
         Intent intent = getIntent();
         String userName = intent.getStringExtra("userName");
         mUserInfo.setText("Welcome "+ userName);
+    }
+
+    private void getRecipes(String recipeName){
+        final YummlyService yummlyService = new YummlyService();
+        yummlyService.findRecipes(recipeName, new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                e.printStackTrace();
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                try {
+                    String jsonData = response.body().string();
+                    Log.v(TAG, jsonData);
+                } catch (IOException e){
+                    e.printStackTrace();
+                }
+            }
+        });
     }
 }
