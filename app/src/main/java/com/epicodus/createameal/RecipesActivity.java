61ec7ef56378765dfb15933b84581e1d58ccb92b
Vanessa Palacios Sharma <vanelunapalacios@live.com>
2016-12-01 10:15:18 -0800

    Add RecipesActivity.java with search engine for recipes.

---
+++ app/src/main/java/com/epicodus/createameal/RecipesActivity.java
@@ -0,0 +1,34 @@
+package com.epicodus.createameal;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+public class RecipesActivity extends AppCompatActivity {
+    @Bind(R.id.recipeLookup) EditText mRecipeLookup;
+    @Bind(R.id.searchButton) Button mSearchButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recipes);
+        ButterKnife.bind(this);
+
+        mSearchButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+             public void onClick(View v){
+                String recipeLookup = mRecipeLookup.getText().toString();
+                Intent intent = new Intent(RecipesActivity.this, RecipesActivity.class);
+                intent.putExtra("recipeLookup", recipeLookup);
+                startActivity(intent);
+            }
+
+        });
+    }
+}
