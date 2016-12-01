85444018b7eee2e063cf85cdf9fece8966df6b62
Vanessa Palacios Sharma <vanelunapalacios@live.com>
2016-12-01 08:55:42 -0800

    Create YummlyService and start Yummly request.

--- app/src/main/java/com/epicodus/createameal/Constants.java
+++ app/src/main/java/com/epicodus/createameal/Constants.java
@@ -1,11 +1,12 @@
 package com.epicodus.createameal;
 
-/**
- * Created by Guest on 11/30/16.
- */
+
 public class Constants {
     public static final String YUMMLY_KEY= BuildConfig.YUMMLY_KEY;
+    public static final String YUMMLY_ID= BuildConfig.YUMMLY_ID;
     public static final String YUMMLY_BASE_URL= " http://api.yummly.com/v1/api/recipes?";
     public static final String YUMMLY_QUERY_PARAMETER = "recipeName";
+    public static final String YUMMLY_KEY_QUERY_PARAMETER="_app_key";
+    public static final String YUMMLY_ID_QUERY_PARAMETER="_app_id";
 }
 
--- app/src/main/java/com/epicodus/createameal/YummlyService.java
+++ app/src/main/java/com/epicodus/createameal/YummlyService.java
@@ -1,6 +1,19 @@
 package com.epicodus.createameal;
 
+import java.net.URL;
+
+import okhttp3.Callback;
+import okhttp3.HttpUrl;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
 
 public class YummlyService {
+    public static void findRecipes(String recipeName, Callback callback) {
 
+        OkHttpClient client = new OkHttpClient.Builder()
+                .build();
+
+        HttpUrl.Builder urlBuilder = HttpUrl.parse(Constants.YUMMLY_BASE_URL).newBuilder();
+        urlBuilder.addQueryParameter(Constants.Yu)
+    }
 }
