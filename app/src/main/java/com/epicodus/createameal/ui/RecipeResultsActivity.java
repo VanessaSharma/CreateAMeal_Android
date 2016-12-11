b55e78beb3d5554a09e42b1d082744bb1e26c31a
Vanessa Palacios Sharma <vanelunapalacios@live.com>
2016-12-11 15:11:45 -0500

    Create SearchView Widget in RecipeResultsActivity.

--- app/src/main/java/com/epicodus/createameal/ui/RecipeResultsActivity.java
+++ app/src/main/java/com/epicodus/createameal/ui/RecipeResultsActivity.java
@@ -1,23 +1,26 @@
 package com.epicodus.createameal.ui;
 
-
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
-import android.widget.TextView;
+import android.support.v7.widget.SearchView;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
-import java.io.IOException;
-import java.util.ArrayList;
-
+import com.epicodus.createameal.Constants;
 import com.epicodus.createameal.R;
 import com.epicodus.createameal.adapters.RecipeListAdapter;
 import com.epicodus.createameal.services.YummlyService;
 import com.epicodus.createameal.models.Recipe;
+
+import java.io.IOException;
+import java.util.ArrayList;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
@@ -26,7 +29,11 @@ import okhttp3.Callback;
 import okhttp3.Response;
 
 public class RecipeResultsActivity extends AppCompatActivity {
+    private SharedPreferences mSharedPreferences;
+    private SharedPreferences.Editor mEditor;
+    private String mRecentRecipe;
     public static final String TAG = RecipeResultsActivity.class.getSimpleName();
+
     @Bind(R.id.recyclerView) RecyclerView mRecyclerView;
 
     private RecipeListAdapter mAdapter;
@@ -41,8 +48,54 @@ public class RecipeResultsActivity extends AppCompatActivity {
 
         Intent intent = getIntent();
         String recipe = intent.getStringExtra("recipe");
+
         getRecipes(recipe);
+
+        mSharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+        mRecentRecipe = mSharedPreferences.getString(Constants.PREFERENCES_NAME_KEY, null);
+
+        if (mRecentRecipe != null) {
+            getRecipes(mRecentRecipe);
+        }
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_search, menu);
+        ButterKnife.bind(this);
+
+        mSharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+        mEditor = mSharedPreferences.edit();
+
+        MenuItem menuItem = menu.findItem(R.id.action_search);
+        SearchView searchView = (SearchView) MenuItemCompat.getActionView(menuItem);
+
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                addToSharedPreferences(query);
+                getRecipes(query);
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                return false;
+            }
+
+        });
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        return super.onOptionsItemSelected(item);
+    }
+
+
 
     private void getRecipes(final String recipeName){
         final YummlyService yummlyService = new YummlyService();
@@ -69,4 +122,9 @@ public class RecipeResultsActivity extends AppCompatActivity {
             }
         });
     }
+
+    private void addToSharedPreferences(String name) {
+        mEditor.putString(Constants.PREFERENCES_NAME_KEY, name).apply();
+    }
+
 }
