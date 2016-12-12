1acd4b552965a06f9d1b2e6024ee2b0f536d8313
Vanessa Palacios Sharma <vanelunapalacios@live.com>
2016-12-12 08:57:57 -0800

    Create overflow menu and update MainActivity layout.

--- app/src/main/java/com/epicodus/createameal/ui/MainActivity.java
+++ app/src/main/java/com/epicodus/createameal/ui/MainActivity.java
@@ -1,16 +1,28 @@
 package com.epicodus.createameal.ui;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.graphics.Typeface;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
-import android.widget.Toast;
 
-
+import com.epicodus.createameal.Constants;
 import com.epicodus.createameal.R;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
@@ -32,7 +44,34 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         mSignInButton.setOnClickListener(this);
         mSignUpButton.setOnClickListener(this);
     }
-            @Override
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.action_logout) {
+            logout();
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void logout() {
+        FirebaseAuth.getInstance().signOut();
+        Intent intent = new Intent(MainActivity.this, LogInActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(intent);
+        finish();
+    }
+
+    @Override
             public void onClick (View v){
                 if(v == mSignInButton) {
                     Intent intent = new Intent(MainActivity.this, LogInActivity.class);
@@ -42,6 +81,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                         startActivity(intent);
                 } else{
                     Intent intent = new Intent(MainActivity.this, MainActivity.class);
+                    startActivity(intent);
                 }
 
     }
