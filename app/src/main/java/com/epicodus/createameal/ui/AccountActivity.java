2c2305d23fdbf5ee11497f90ff97614ba6452e4f
Vanessa Palacios Sharma <vanelunapalacios@live.com>
2016-12-12 07:31:54 -0800

    Replace old AccountActivity and LogInActivity for new ones and set up individual xml.

--- app/src/main/java/com/epicodus/createameal/ui/AccountActivity.java
+++ app/src/main/java/com/epicodus/createameal/ui/AccountActivity.java
@@ -1,41 +1,15 @@
 package com.epicodus.createameal.ui;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
 
 import com.epicodus.createameal.R;
 
-import butterknife.Bind;
-import butterknife.ButterKnife;
-
 public class AccountActivity extends AppCompatActivity {
-    @Bind(R.id.userName) EditText mUserName;
-    @Bind(R.id.email) EditText mEmail;
-    @Bind(R.id.password) EditText mPassword;
-    @Bind(R.id.createButton)
-    Button mCreateButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_account);
-        ButterKnife.bind(this);
-
-        mCreateButton.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View v){
-                String userName = mUserName.getText().toString();
-                String email = mEmail.getText().toString();
-                String password = mPassword.getText().toString();
-                Intent intent = new Intent(AccountActivity.this, UserActivity.class);
-                intent.putExtra("userName", userName);
-                startActivity(intent);
-
-            }
-        });
     }
 }
--- app/src/main/java/com/epicodus/createameal/ui/LogInActivity.java
+++ app/src/main/java/com/epicodus/createameal/ui/LogInActivity.java
@@ -1,39 +1,15 @@
 package com.epicodus.createameal.ui;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
 
 import com.epicodus.createameal.R;
 
-import butterknife.Bind;
-import butterknife.ButterKnife;
-
 public class LogInActivity extends AppCompatActivity {
-    @Bind(R.id.userName) EditText mUserName;
-    @Bind(R.id.userPassword) EditText mUserPassword;
-    @Bind(R.id.logInButton) Button mLogInButton;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log_in);
-        ButterKnife.bind(this);
-
-        mLogInButton.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View v){
-                String userName = mUserName.getText().toString();
-                String password = mUserPassword.getText().toString();
-                Intent intent = new Intent(LogInActivity.this, UserActivity.class);
-                intent.putExtra("userName", userName);
-                startActivity(intent);
-
-            }
-        });
     }
 }
