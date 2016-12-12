7a4033bf719c73571fde470533a724dcb8aebefb
Vanessa Palacios Sharma <vanelunapalacios@live.com>
2016-12-12 08:23:51 -0800

    Create account registration for new users.

--- app/src/main/java/com/epicodus/createameal/ui/LogInActivity.java
+++ app/src/main/java/com/epicodus/createameal/ui/LogInActivity.java
@@ -1,15 +1,34 @@
 package com.epicodus.createameal.ui;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import com.epicodus.createameal.R;
 
-public class LogInActivity extends AppCompatActivity {
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+public class LogInActivity extends AppCompatActivity implements View.OnClickListener {
+    @Bind(R.id.registerTextView) TextView mRegisterTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log_in);
+
+        ButterKnife.bind(this);
+        mRegisterTextView.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view == mRegisterTextView) {
+            Intent intent = new Intent(LogInActivity.this, AccountActivity.class);
+            startActivity(intent);
+            finish();
+        }
     }
 }
