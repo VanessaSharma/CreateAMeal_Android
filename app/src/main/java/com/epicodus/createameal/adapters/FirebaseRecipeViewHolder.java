2eb29571c240a5b6248fcce4bc205e6dd8b8b485
Vanessa Palacios Sharma <vanelunapalacios@live.com>
2016-12-20 10:22:52 -0800

    Inflate animators and implement ItemTouchHelperViewHolder interface.

--- app/src/main/java/com/epicodus/createameal/adapters/FirebaseRecipeViewHolder.java
+++ app/src/main/java/com/epicodus/createameal/adapters/FirebaseRecipeViewHolder.java
@@ -1,5 +1,7 @@
 package com.epicodus.createameal.adapters;
 
+import android.animation.AnimatorInflater;
+import android.animation.AnimatorSet;
 import android.content.Context;
 import android.content.Intent;
 import android.support.v7.widget.RecyclerView;
@@ -11,6 +13,8 @@ import com.epicodus.createameal.Constants;
 import com.epicodus.createameal.R;
 import com.epicodus.createameal.models.Recipe;
 import com.epicodus.createameal.ui.RecipeDetailActivity;
+import com.epicodus.createameal.util.ItemTouchHelperAdapter;
+import com.epicodus.createameal.util.ItemTouchHelperViewHolder;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -22,7 +26,7 @@ import org.parceler.Parcels;
 
 import java.util.ArrayList;
 
-public class FirebaseRecipeViewHolder extends RecyclerView.ViewHolder {
+public class FirebaseRecipeViewHolder extends RecyclerView.ViewHolder implements ItemTouchHelperViewHolder {
     private static final int MAX_WIDTH = 200;
     private static final int MAX_HEIGHT = 200;
     public ImageView mRecipeImageView;
@@ -52,4 +56,21 @@ public class FirebaseRecipeViewHolder extends RecyclerView.ViewHolder {
         ingredientsTextView.setText(recipe.getIngredients().get(0));
         ratingTextView.setText("Rating: " + recipe.getRating() + "/5");
     }
+
+    @Override
+    public void onItemSelected() {
+    AnimatorSet set = (AnimatorSet) AnimatorInflater.loadAnimator(mContext,
+            R.animator.drag_scale_on);
+    set.setTarget(itemView);
+    set.start();
+}
+
+    @Override
+    public void onItemClear() {
+        AnimatorSet set = (AnimatorSet) AnimatorInflater.loadAnimator(mContext,
+                R.animator.drag_scale_off);
+        set.setTarget(itemView);
+        set.start();
+    }
+
 }
---
+++ app/src/main/java/com/epicodus/createameal/util/ItemTouchHelperViewHolder.java
@@ -0,0 +1,9 @@
+package com.epicodus.createameal.util;
+
+/**
+ * Created by Guest on 12/20/16.
+ */
+public interface ItemTouchHelperViewHolder {
+    void onItemSelected();
+    void onItemClear();
+}
