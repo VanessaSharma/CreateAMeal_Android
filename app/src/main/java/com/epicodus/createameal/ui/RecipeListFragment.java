fee68249ca89d65a0a7070f3c692523a066651fb
Vanessa Palacios Sharma <vanelunapalacios@live.com>
2016-12-20 12:47:21 -0800

    Create RecipeListFragment and SavedRecipeListFragment.

---
+++ app/src/main/java/com/epicodus/createameal/ui/RecipeListFragment.java
@@ -0,0 +1,131 @@
+package com.epicodus.createameal.ui;
+
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.support.v4.app.Fragment;
+import android.support.v4.view.MenuItemCompat;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.SearchView;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.epicodus.createameal.Constants;
+import com.epicodus.createameal.R;
+import com.epicodus.createameal.adapters.RecipeListAdapter;
+import com.epicodus.createameal.models.Recipe;
+import com.epicodus.createameal.services.YummlyService;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.Response;
+
+public class RecipeListFragment extends Fragment {
+    @Bind(R.id.recyclerView) RecyclerView mRecyclerView;
+
+    private RecipeListAdapter mAdapter;
+    public ArrayList<Recipe> mRecipes = new ArrayList<>();
+    private SharedPreferences mSharedPreferences;
+    private SharedPreferences.Editor mEditor;
+    private String mRecentRecipe;
+
+    public RecipeListFragment() {
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        mSharedPreferences = PreferenceManager.getDefaultSharedPreferences(getActivity());
+        mEditor = mSharedPreferences.edit();
+
+        setHasOptionsMenu(true);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_recipe_list, container, false);
+        ButterKnife.bind(this, view);
+        mRecentRecipe = mSharedPreferences.getString(Constants.PREFERENCES_NAME_KEY, null);
+
+        if (mRecentRecipe != null) {
+            getRecipes(mRecentRecipe);
+        }
+
+        return view;
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        super.onCreateOptionsMenu(menu, inflater);
+        inflater.inflate(R.menu.menu_search, menu);
+
+        MenuItem menuItem = menu.findItem(R.id.action_search);
+        SearchView searchView = (SearchView) MenuItemCompat.getActionView(menuItem);
+
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                addToSharedPreferences(query);
+                getRecipes(query);
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                return false;
+            }
+        });
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void getRecipes(String name) {
+        final YummlyService yummlyService = new YummlyService();
+
+        yummlyService.findRecipes(name, new Callback() {
+
+            @Override
+            public void onFailure(Call call, IOException e) {
+                e.printStackTrace();
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) {
+                mRecipes = yummlyService.processResults(response);
+
+                getActivity().runOnUiThread(new Runnable() {
+
+                    @Override
+                    public void run() {
+                        mAdapter = new RecipeListAdapter(getActivity(), mRecipes);
+                        mRecyclerView.setAdapter(mAdapter);
+                        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getActivity());
+                        mRecyclerView.setLayoutManager(layoutManager);
+                        mRecyclerView.setHasFixedSize(true);
+                    }
+                });
+            }
+        });
+    }
+
+    private void addToSharedPreferences(String location) {
+        mEditor.putString(Constants.PREFERENCES_NAME_KEY, location).apply();
+    }
+
+}
---
+++ app/src/main/java/com/epicodus/createameal/ui/SavedRecipeListFragment.java
@@ -0,0 +1,88 @@
+
+package com.epicodus.createameal.ui;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.helper.ItemTouchHelper;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.epicodus.createameal.Constants;
+import com.epicodus.createameal.R;
+import com.epicodus.createameal.adapters.FirebaseRecipeListAdapter;
+import com.epicodus.createameal.adapters.FirebaseRecipeViewHolder;
+import com.epicodus.createameal.models.Recipe;
+import com.epicodus.createameal.util.OnStartDragListener;
+import com.epicodus.createameal.util.SimpleItemTouchHelperCallback;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+public class SavedRecipeListFragment extends Fragment implements OnStartDragListener {
+    @Bind(R.id.recyclerView) RecyclerView mRecyclerView;
+
+    private FirebaseRecipeListAdapter mFirebaseAdapter;
+    private ItemTouchHelper mItemTouchHelper;
+
+    public SavedRecipeListFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_saved_recipe_list, container, false);
+        ButterKnife.bind(this, view);
+        setUpFirebaseAdapter();
+        return view;
+    }
+
+    private void setUpFirebaseAdapter() {
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        String uid = user.getUid();
+
+        Query query = FirebaseDatabase.getInstance()
+                .getReference(Constants.FIREBASE_CHILD_RECIPES)
+                .child(uid)
+                .orderByChild(Constants.FIREBASE_QUERY_INDEX);
+
+        mFirebaseAdapter = new FirebaseRecipeListAdapter(Recipe.class,
+                R.layout.recipe_list_item_drag, FirebaseRecipeViewHolder.class,
+                query, this, getActivity());
+
+        mRecyclerView.setHasFixedSize(true);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        mRecyclerView.setAdapter(mFirebaseAdapter);
+
+        mFirebaseAdapter.registerAdapterDataObserver(new RecyclerView.AdapterDataObserver() {
+            @Override
+            public void onItemRangeInserted(int positionStart, int itemCount) {
+                super.onItemRangeInserted(positionStart, itemCount);
+                mFirebaseAdapter.notifyDataSetChanged();
+            }
+        });
+
+        ItemTouchHelper.Callback callback = new SimpleItemTouchHelperCallback(mFirebaseAdapter);
+        mItemTouchHelper = new ItemTouchHelper(callback);
+        mItemTouchHelper.attachToRecyclerView(mRecyclerView);
+    }
+
+    @Override
+    public void onStartDrag(RecyclerView.ViewHolder viewHolder) {
+        mItemTouchHelper.startDrag(viewHolder);
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        mFirebaseAdapter.cleanup();
+    }
+
+}
